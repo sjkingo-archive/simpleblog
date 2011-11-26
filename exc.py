5315d3216596e3b312adb17e99644104d7f56d5e
Sam Kingston <sam@sjkwi.com.au>
2011-11-27 09:18:52 +1000

    expand these out to proper exceptions

--- exc.py
+++ exc.py
@@ -1,16 +1,23 @@
-
 class InvalidEntry(Exception):
     def __init__(self, msg):
         self.msg = msg
-
     def __str__(self):
         return self.msg
-InvalidFilter = InvalidEntry
-InvalidConverter = InvalidEntry
+
+class InvalidFilter(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
+
+class InvalidConverter(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
 
 class UnknownEntryType(Exception):
     def __init__(self, t):
         self.msg = '%s is not known to the dispatcher' % t
-
     def __str__(self):
         return self.msg
